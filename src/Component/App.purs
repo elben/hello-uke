4587bcf4501a3fab72f37f6ac95e51f220f60daa
Elben Shira <elbenshira@gmail.com>
2019-03-10 13:18:08 -1000

    Start on State being a list of chords.

--- src/Component/App.purs
+++ src/Component/App.purs
@@ -1,21 +1,26 @@
 module Component.App where
 
 import Prelude
-import Chords as C
 
+import Chords as C
 import Component.ChordSelector as CS
 import Component.Fretboard as FB
+import Data.Array as A
 import Data.Either.Nested (Either2)
 import Data.Functor.Coproduct.Nested (Coproduct2)
-import Data.Maybe (Maybe(..))
+import Data.Maybe (Maybe(..), maybe)
 import Halogen as H
 import Halogen.Component.ChildPath as CP
 import Halogen.HTML as HH
 import Halogen.HTML.Events as HE
 
+data Chord = Chord C.Note C.ChordQuality C.ChordInterval
+
 data State
-  = NoChord
-  | Chord C.Note C.ChordQuality C.ChordInterval
+  = State (Array Chord)
+
+getStateChords :: State -> Array Chord
+getStateChords (State chords) = chords
 
 data Query a
   = HandleChordSelector CS.Message a
@@ -41,14 +46,13 @@ component =
   initialState :: State
   initialState =
     case CS.initialState of
-      CS.Chord (Just n) (Just q) (Just i) -> Chord n q i
-      _ -> NoChord
+      CS.Chord (Just n) (Just q) (Just i) -> State [Chord n q i]
+      _ -> State []
 
   render :: State -> H.ParentHTML Query ChildQuery ChildSlot m
   render state =
-    let fbState = case state of
-                    NoChord -> FB.NoChordInput
-                    Chord n q i -> FB.ChordInput n q i
+  -- TODO make this render ALL the chords in the state. Need to use multi-slots, like the Todo app?
+    let fbState = maybe FB.NoChordInput (\(Chord n q i) -> FB.ChordInput n q i) (A.index (getStateChords state) 0)
     in HH.div_
         [ HH.slot' CP.cp1 unit CS.component unit (HE.input HandleChordSelector)
         , HH.slot' CP.cp2 unit FB.component fbState (HE.input HandleFretboard)
@@ -59,7 +63,7 @@ component =
     HandleChordSelector (CS.ChordSelected note quality interval) next -> do
       -- clear <- H.query' CP.cp1 unit (H.request CS.Clear)
       -- H.modify_ (\st -> st { toggleCount = st.toggleCount + 1 })
-      H.put (Chord note quality interval)
+      H.put (State [Chord note quality interval])
       pure next
     HandleChordSelector (CS.ChordAdded note quality interval) next -> do
       -- TODO
