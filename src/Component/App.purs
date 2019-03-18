a76283626a37627ba961cdebf5bbffd16b48b85f
Elben Shira <elbenshira@gmail.com>
2019-03-17 23:12:37 -1000

    Add new "active fretboard" component.
    Use more CSS flex.

--- src/Component/App.purs
+++ src/Component/App.purs
@@ -5,10 +5,11 @@ module Component.App where
 import Prelude
 
 import Component.ChordSelector as CS
+import Component.Fretboard as FB
 import Component.Fretboards as FBS
 import Data.Array as A
-import Data.Either.Nested (Either2)
-import Data.Functor.Coproduct.Nested (Coproduct2)
+import Data.Either.Nested (Either3)
+import Data.Functor.Coproduct.Nested (Coproduct3)
 import Data.Maybe (Maybe(..))
 import Halogen (ClassName(..))
 import Halogen as H
@@ -22,12 +23,13 @@ type State = { chord :: M.Chord, chords :: Array M.Chord }
 
 data Query a
   = HandleChordSelector CS.Message a
+  | HandleFretboard FB.Message a
   | HandleFretboards FBS.Message a
   | AddChord a
 
-type ChildQuery = Coproduct2 CS.Query FBS.Query
+type ChildQuery = Coproduct3 CS.Query FB.Query FBS.Query
 
-type ChildSlot = Either2 Unit Unit
+type ChildSlot = Either3 Unit Unit Unit
 
 newtype FretboardSlot = FretboardSlot Int
 
@@ -49,24 +51,33 @@ component =
 
   render :: State -> H.ParentHTML Query ChildQuery ChildSlot m
   render state =
-    HH.div_
+    HH.div
+      [ HP.classes [ClassName "main-component"] ]
       [ HH.div
-          [ HP.classes [ClassName "chord-selector-section"] ]
+          [ HP.classes [ClassName "top-component"] ]
+          [ HH.div
+              [ HP.classes [ClassName "chord-selector-section"] ]
 
-          -- Render the ChordSelector component. It doesn't have any inputs to it, so pass in
-          -- unit as the input. It emits a message whenever a valid chord is selected, which is
-          -- passed via the HandleChordSelector wrapper.
-          [ HH.slot' CP.cp1 unit CS.component unit (HE.input HandleChordSelector)
+              -- Render the ChordSelector component. It doesn't have any inputs to it, so pass in
+              -- unit as the input. It emits a message whenever a valid chord is selected, which is
+              -- passed via the HandleChordSelector wrapper.
+              [ HH.slot' CP.cp1 unit CS.component unit (HE.input HandleChordSelector)
 
-          -- Render the [Add] button.
-          , HH.div
-              [ HP.classes [ClassName "selection", ClassName "wide", ClassName "btn"]
-              , HE.onClick (HE.input_ AddChord) ]
-              [ HH.text "Add" ]
+              -- Render the [Add] button.
+              , HH.div
+                  [ HP.classes [ClassName "selection", ClassName "wide", ClassName "btn"]
+                  , HE.onClick (HE.input_ AddChord) ]
+                  [ HH.text "Add" ]
+              ]
+
+            -- Render the current fretboard.
+            , HH.div
+                [ HP.classes [ ClassName "fretboard-active" ] ]
+                [ HH.slot' CP.cp2 unit FB.component (FB.ChordInput state.chord) (HE.input HandleFretboard) ]
           ]
 
       -- Render all the fretboards. Passes in the list of chords to render as input to the fretboard.
-      , HH.slot' CP.cp2 unit FBS.component (FBS.FretboardChords (A.cons state.chord state.chords)) (HE.input HandleFretboards)
+      , HH.slot' CP.cp3 unit FBS.component (FBS.FretboardChords state.chords) (HE.input HandleFretboards)
       ]
 
   eval :: Query ~> H.ParentDSL State Query ChildQuery ChildSlot Void m
@@ -75,6 +86,8 @@ component =
       H.modify_ (_ { chord = chord })
       pure next
     HandleChordSelector CS.NoMessage next -> do
+      pure next
+    HandleFretboard m next -> do
       pure next
     HandleFretboards m next -> do
       pure next
